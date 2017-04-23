7265934395ecb380fc45aea3d1388910245090ea
Hidde Bultsma <dodo@dodomail.org>
2017-04-23 20:50:27 +0200

    Add experimental support for WAV-files in cli

--- shipper/cli.py
+++ shipper/cli.py
@@ -6,21 +6,28 @@ import simplecrypt
 import sys
 from .helpers import prompt_password
 from .pickers import RandomPicker
-from .shipments import LosslessImageShipment
+from .shipments import LosslessImageShipment, WaveShipment
 
 
 #: The default seed used for the RandomPicker
 DEFAULT_SEED = 7
 
 
+CARGO_TYPES = {
+    'png': LosslessImageShipment,
+    'wav': WaveShipment,
+}
+
+
+def get_shipment_method(container):
+    for cargo_type, shipment in CARGO_TYPES.items():
+        if container.lower().endswith('.{}'.format(cargo_type)):
+            return shipment
+
+
 @click.group()
 def cli():
     pass
-
-
-def validate_pass(ctx, param, value):
-    print(ctx, param, value)
-    return value
 
 
 @cli.command()
@@ -29,6 +36,7 @@ def validate_pass(ctx, param, value):
 @click.option('--cargo', '-c', type=click.File('rb'), default=sys.stdin.buffer)
 @click.option('--lock', '-l', is_flag=True, default=False)
 def load(container, destination, cargo, lock):
+    shipment_method = get_shipment_method(container)
     seed = DEFAULT_SEED
     cargo = cargo.read()
 
@@ -38,7 +46,7 @@ def load(container, destination, cargo, lock):
         cargo = simplecrypt.encrypt(password, cargo)
         del password
 
-    shipment = LosslessImageShipment(container)
+    shipment = shipment_method(container)
     shipment.load(cargo, picker=RandomPicker(seed))
     shipment.ship(destination)
 
@@ -49,13 +57,14 @@ def load(container, destination, cargo, lock):
               default=sys.stdout.buffer)
 @click.option('--unlock', '-l', is_flag=True, default=False)
 def unload(container, destination, unlock):
+    shipment_method = get_shipment_method(container)
     seed = DEFAULT_SEED
 
     if unlock:
         password = prompt_password()
         seed = hashlib.sha512(password.encode('utf-8')).hexdigest()
 
-    shipment = LosslessImageShipment(container)
+    shipment = shipment_method(container)
     cargo = shipment.unload(picker=RandomPicker(seed))
 
     if unlock:
